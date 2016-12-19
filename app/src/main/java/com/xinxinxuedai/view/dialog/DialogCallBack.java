5e71fe4a43dda610abbe1b21e8c42acb92548c84
玉田 <358769601@qq.com>
2016-12-19 21:00:08 +0800

    Upload DialogCallBack.java

---
+++ app/src/main/java/com/xinxinxuedai/view/dialog/DialogCallBack.java
@@ -0,0 +1,15 @@
+package com.xinxinxuedai.view.dialog;
+
+/**
+ * Created by 35876 于萌萌
+ * 创建日期: 19:42 . 2016年12月19日
+ * 描述:asdsss
+ * <p>sss
+ * <p>
+ * 备注:
+ */
+
+public interface DialogCallBack {
+    void confirm();
+    void cancel();
+}
